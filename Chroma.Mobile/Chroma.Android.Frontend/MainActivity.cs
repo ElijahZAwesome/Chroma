c46d7a703d3766040f7e21d11f035c067706f27f
Enova <17408285+Enovale@users.noreply.github.com>
2022-12-26 02:43:34 -0600

    Fix bindings generation and fixed deprecated fullscreen.

--- Chroma.Mobile/Chroma.Android.Frontend/MainActivity.cs
+++ Chroma.Mobile/Chroma.Android.Frontend/MainActivity.cs
@@ -1,6 +1,5 @@
 using Android.Content.PM;
 using Android.Views;
-
 using Org.Libsdl.App;
 
 namespace Chroma.Android.Frontend;
@@ -14,30 +13,24 @@ namespace Chroma.Android.Frontend;
 )]
 public class MainActivity : SDLActivity
 {
-    public static MainActivity ActivityInstance { get; protected set; }
+    public static MainActivity ActivityInstance { get; private set; }
 
     public static bool Fullscreen = true;
+
+    public MainActivity()
+    {
+        ActivityInstance = this;
+    }
 
     public override void LoadLibraries()
     {
         base.LoadLibraries();
-        ActivityInstance = this;
         Bootstrap.SetupMain();
     }
 
     public override void OnWindowFocusChanged(bool hasFocus)
     {
         base.OnWindowFocusChanged(hasFocus);
-        if (hasFocus && Fullscreen)
-        {
-            Window!.DecorView.SystemUiVisibility = (StatusBarVisibility)(
-                SystemUiFlags.LayoutStable |
-                SystemUiFlags.LayoutHideNavigation |
-                SystemUiFlags.LayoutFullscreen |
-                SystemUiFlags.HideNavigation |
-                SystemUiFlags.Fullscreen |
-                SystemUiFlags.ImmersiveSticky
-            );
-        }
+        Window!.SetDecorFitsSystemWindows(!(hasFocus && Fullscreen));
     }
 }
